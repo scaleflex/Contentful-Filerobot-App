6e93400fae63f37c49580545e958533c201bd06a
root <root@Hiep>
2024-09-18 18:37:19 +0700

    update limit

--- src/locations/Field.jsx
+++ src/locations/Field.jsx
@@ -21,6 +21,7 @@ const Field = () => {
     const [configs, setConfigs] = useState({})
     const [endpoint, setEndpoint] = useState('')
     const [isLoading, setIsLoading] = useState(false)
+    const [isHasLimit, setIsHasLimit] = useState(false)
 
     const styles = {
         card: css({
@@ -59,9 +60,11 @@ const Field = () => {
     useEffect(() => {
         let storedAssets = sdk.field.getValue();
         storedAssets = storedAssets ? storedAssets : [];
+
         if (storedAssets.length > 0) {
             setAssets(storedAssets)
         }
+
         setConfigs(sdk.parameters.installation)
     }, [sdk]);
 
@@ -69,6 +72,10 @@ const Field = () => {
         if ('token' in configs) {
             setEndpoint('https://api.filerobot.com/' + configs.token + '/v5')
         }
+
+        if ('limit' in configs && configs?.limit > 0) setIsHasLimit(true)
+        else setIsHasLimit(false)
+
     }, [configs]);
 
       // Function to fetch data from an API
@@ -196,13 +203,13 @@ const Field = () => {
             <Card
                 className={styles.card}
                 dragHandleRender={() => (
-                <DragHandle
-                    as="button"
-                    className={styles.dragHandle}
-                    label="Move card"
-                    {...attributes}
-                    {...listeners}
-                />
+                    <DragHandle
+                        as="button"
+                        className={styles.dragHandle}
+                        label="Move card"
+                        {...attributes}
+                        {...listeners}
+                    />
                 )}
                 padding="none"
                 withDragHandle
@@ -235,7 +242,7 @@ const Field = () => {
             const oldIndex = assetsMapId.indexOf(active.id);
             const newIndex = assetsMapId.indexOf(over.id);
             return arrayMove(assets, oldIndex, newIndex);
-          });
+          }, () => { sdk.field.setValue(assets).then((data) => sdk.entry.save())});
         }
       };
 
